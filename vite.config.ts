90f6939ea16fea5918b51957fe604768db2b3c83
maweiyi <201250225@smail.nju.edu.cn>
2024-04-01 17:34:37 +0800

    framwork

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueSetupExtend(),
   ],
   resolve: {
     alias: {
